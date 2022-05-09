e734bfcb92d94d63d5a5c7399fecf6f0a3ca28cc
Anthony Levaufre <anthony@nonsenz.org>
2022-05-09 20:09:37 +0200

    weather2

--- users/migrations/0016_rename_wdate_weather_wdate0_rename_wid_weather_wid0_and_more.py
+++ users/migrations/0016_rename_wdate_weather_wdate0_rename_wid_weather_wid0_and_more.py
@@ -57,7 +57,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='weather',
             name='user',
-            field=models.OneToOneField(default=django.utils.timezone.now, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.OneToOneField(default=1, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AddField(
