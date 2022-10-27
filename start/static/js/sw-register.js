33917851fbf0bbbd7ed178040866665b23153ec9
Anthony Levaufre <anthony@nonsenz.org>
2022-10-27 22:10:17 +0200

    pwa attempt 1

--- start/static/js/sw-register.js
+++ start/static/js/sw-register.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/static/js/sw.js");
+    navigator.serviceWorker.register("/sw.js");
   }
 });
