38aa1143c8457db8d12a3532ecad2ea0d7edb446
Anthony Levaufre <anthony@nonsenz.org>
2022-10-27 21:22:57 +0200

    pwa attempt 1

--- start/urls.py
+++ start/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 urlpatterns = [
-   path('',views.home),
-
+   path('', views.home),
+   path('sw.js', views.service_worker),
+   path('offline/', TemplateView.as_view(template_name="start/offline.html")),
 ]
--- start/views.py
+++ start/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from django.http import HttpResponse
+
 
 def home(request):
-   if request.user.is_authenticated:
-      return redirect('/core/')
-   else:
-      content = {
-         'titre': _("Bonsaï management"),
-         'head1': _("what?")
-      }
+    if request.user.is_authenticated:
+        return redirect('/core/')
+    else:
+        content = {
+            'titre': _("Bonsaï management"),
+            'head1': _("what?")
+        }
 
-      context = {'content': content}
-      return render(request,'start/home.html',context)
+        context = {'content': content}
+        return render(request, 'start/home.html', context)
 
+
+def service_worker(request):
+    sw_path = settings.BASE_DIR / "static/js" / "sw.js"
+    response = HttpResponse(open(sw_path).read(), content_type='application/javascript')
+    return response
